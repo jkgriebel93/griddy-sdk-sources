35ef6096563e607f10ab9f7c30843d9eefc0835b
John Griebel <jkgriebel93@gmail.com>
2025-10-10 10:45:23 -0400

    Script updates
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- scripts/gen_and_diff_sdk.sh
+++ scripts/gen_and_diff_sdk.sh
@@ -13,6 +13,17 @@ curl -fsSL https://raw.githubusercontent.com/speakeasy-api/speakeasy/main/instal
 speakeasy --version
 
 cd $HOME
+ls -lah
+ls -lah work
 echo "Operating in $(pwd)"
 echo "Using GitHub CLI to clone Python SDK Repo..."
 gh repo clone $PYTHON_SDK_REPO
+
+
+cd $GITHUB_WORKSPACE
+echo "Now operating in ${GITHUB_WORKSPACE}"
+ls -lah
+
+echo "Going to attempt to generate SDK now"
+speakeasy generate sdk --lang python --schema openapi/nfl-com-api.yaml --out griddy-sdk-python
+
