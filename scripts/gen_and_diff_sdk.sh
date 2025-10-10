8176ce1245b39f16efa196cca0ea14917a452943
John Griebel <jkgriebel93@gmail.com>
2025-10-10 11:14:54 -0400

    Fix repo name
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- scripts/gen_and_diff_sdk.sh
+++ scripts/gen_and_diff_sdk.sh
@@ -18,10 +18,10 @@ ls -lah
 ls -lah work
 echo "Operating in $(pwd)"
 echo "Using GitHub CLI to clone Python SDK Repo..."
-gh repo clone 'griddy-sdk-python'
+gh repo clone "${PYTHON_SDK_REPO}"
 
 
-cd $GITHUB_WORKSPACE
+cd "${GITHUB_WORKSPACE}"
 echo "Now operating in ${GITHUB_WORKSPACE}"
 ls -lah
 
