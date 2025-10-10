bd615661da5da0d84f08458ea1634fbc017b9497
John Griebel <jkgriebel93@gmail.com>
2025-10-10 10:36:27 -0400

    Attempt to checkout sdk repo
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- scripts/gen_and_diff_sdk.sh
+++ scripts/gen_and_diff_sdk.sh
@@ -4,10 +4,15 @@ set -e
 echo "Current working directory: $(pwd)"
 echo "GitHub workspace: ${GITHUB_WORKSPACE}"
 echo "Python SDK Repo: ${PYTHON_SDK_REPO}"
+echo "HOME: ${HOME}"
 echo "Python Version: $(python --version)"
-ls -lah
+
 
 echo "Installing Speakeasy CLI..."
 curl -fsSL https://raw.githubusercontent.com/speakeasy-api/speakeasy/main/install.sh | bash -s -- -b /usr/local/bin
 speakeasy --version
 
+cd $HOME
+echo "Operating in $(pwd)"
+echo "Using GitHub CLI to clone Python SDK Repo..."
+gh repo clone $PYTHON_SDK_REPO
